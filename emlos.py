1612561399678470a53cd435b74f84fd248cb98e
Gerinova01 <kullai.gergo@diak.szbi-pg.hu>
2026-03-11 08:45:59 +0100

    kutya, cica upgrade pack

--- emlos.py
+++ emlos.py
@@ -6,3 +6,18 @@ class Emlos(Allat):
 
     def __str__(self):
         return f"{self.nev} {self.faj} {self.eletkor} éves élőhelye {self.elohely}, szőrzete {self.szorzet_szine} "
+
+class Macska(Emlos):
+    def __init__(self, nev, eletkor, elohely, szorzet_szine):
+        super().__init__(nev, "macska", eletkor, elohely, szorzet_szine)
+
+    def doromboral(self):
+        print(f"{self.nev} épp doromborál")
+
+
+class Kutya(Emlos):
+    def __init__(self, nev, eletkor, elohely, szorzet_szine):
+        super().__init__(nev, "kutya", eletkor, elohely, szorzet_szine)
+
+    def ugatol(self):
+        print(f"{self.nev} épp ugatol")
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from allat import Allat
 from emlos import Emlos
+from emlos import Macska
+from emlos import Kutya
 
 allat1 = Allat("Bodri", "kutya", 5, "kert", "közepes")
 allat2 = Allat("Cirmi", "macska", 3, "ház", "közepes")
@@ -7,4 +9,16 @@ allat2 = Allat("Cirmi", "macska", 3, "ház", "közepes")
 print(allat1)
 print(allat2)
 
-emlos1 = Emlos("Cápali", "kutya", 3, "ház", "közepes", "szürke")
+emlos1 = Emlos("Capali", "kutya", 3, "ház", "szürke")
+emlos2 = Emlos("Capeti", "macska", 7, "ház", "fekete")
+
+print(emlos1)
+print(emlos2)
+
+macska1 = Macska("Hubert", 4, "ház", "fehér")
+print(macska1)
+macska1.doromboral()
+
+kutya1 = Kutya("Bongyor", 9, "kert", "szürke")
+print(kutya1)
+kutya1.ugatol()
